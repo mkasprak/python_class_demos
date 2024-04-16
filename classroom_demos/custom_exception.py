4e9c26fb3747009f8d008872e6d834fc6feec896
mkasprak <mkasprak@mchenry.edu>
2024-04-16 11:01:52 -0500

    custom exception

--- classroom_demos/custom_exception.py
+++ classroom_demos/custom_exception.py
@@ -1,23 +1,56 @@
-class InvalidInputError(ValueError):
-    # Custom exception implementation
-    print("Naughty naughty! You were not supposed to do that")
+# class InvalidInputError(ValueError):
+#     # Custom exception implementation
+#     print("Naughty naughty! You were not supposed to do that")
+
+
+# def main():
+#     try:
+#         value = int(input("Please enter a number between 0 and 9 "))
+#         print(f"You entered: {value}")
+
+#     except ValueError:
+#         print("Shame")
+#     except Exception as e:
+#         print(e.__str__)
+#         print("You need to input a whole number.")
+#         main()
+#     else:
+#         print("Thank you for entering a valid number.")
+#     finally:
+#         print("Thank's for playing!")
+9.2
+
+# main()
+
+# 7.2, Meri ValueError
+
+
+class InvalidInputError(Exception):
+    def __init__(self, message="Can't you read the directions? LESS than 10."):
+        self.message = message
+        super().__init__(self.message)
+
+    def __str__(self):
+        return f'{self.message}'
 
 
 def main():
     try:
-        value = int(input("Please enter a number between 0 and 9 "))
-        print(f"You entered: {value}")
+        # goal - create a custom error for if you get a float when you want an int
+        value = int(input("Please enter a number from 0 to 9: "))
+        if value > 9:
+            raise InvalidInputError()
 
-    except ValueError:
-        print("Shame")
+    except InvalidInputError as e:
+        print(f"error: {e}")
     except Exception as e:
-        print(e.__str__)
-        print("You need to input a whole number.")
+        print(f"The exception is: {e}")
+        print("Please enter an integer between 0 and 9")
         main()
     else:
-        print("Thank you for entering a valid number.")
+        print(f"Thank you, you entered {value}.")
     finally:
-        print("Thank's for playing!")
+        print("Finally!!!")
 
 
 main()
