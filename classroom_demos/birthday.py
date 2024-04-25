f1ee4fd25f46c95454e760a961de1036789da33b
mkasprak <mkasprak@mchenry.edu>
2024-04-25 10:42:51 -0500

    date time

--- classroom_demos/birthday.py
+++ classroom_demos/birthday.py
@@ -1,33 +1,30 @@
 # calculate a person's age in: Years, months, weeks, days
+# 30.41 days in a month
+# 7 days in a week 
 
 from datetime import datetime
 
 def main():
-    print("\n\n")
-    # now = datetime.now()
-    # print(f"now: {now}")
-    # timestamp = datetime.now().time()
-    # print(f"timestamp: {timestamp}")
+   
     print("\n\n")
     try:
         today = datetime.today()
-        print(today.weekday())
         birth_year = int(input("What year were you born?  "))
         month = int(input("What Month were you born (as a number. May would be 5)  "))
         day = int(input("What day of the month were you born?  "))
         # just need datetime not datetime.date
         # because we imported datetime from datetime
         birthday = datetime(birth_year, month, day)
-        birth_year = str(birth_year + 10)
-       
         print("Your birthday is: ")
         birthday_output = birthday.strftime("%Y-%m-%d")
         print(birthday_output) 
-        birthday = birthday.replace(year = 1981)
-        print (f"Gee, you look like you were born {birthday}")
-        date_string = str(birth_year) + "-" + str(month) + "-" + str(day) 
-        dt = datetime.strptime(date_string, "%Y-%m-%d")
-        print(dt)
+
+        delta = today - birthday
+        print(f'Difference is {delta.days} days')
+        delta_years = delta.days // 365.2425
+       
+        print(f'You are {delta_years} old')
+       
       
     except Exception as e:
         print(f"ooooops!!!:  {e}") 
